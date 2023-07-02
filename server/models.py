a7ad668f29ba3d6d65c9976ca616ace8c36b7828
vincenttommi <vincenttommi@gmail.com>
2023-07-02 11:00:03 +0300

    models complted

--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.column(db.String())
+    image  = db.column(db.String())
+    price  = db.column(db.Float())
+    is_in_stock = db.column(db.Boolean)
+
+
+def __repr__(self): 
+    return  f'<plant {self.name} | In Stock:{self.is_in_stock}'
+
+          
+    
